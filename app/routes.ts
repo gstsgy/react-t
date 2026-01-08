e43486ea4e4ce80db5f9e8998c033c17b1581b60
122759654 <122759654@qq.com>
2026-01-08 01:52:37 +0000

    ./试一下

--- app/routes.ts
+++ app/routes.ts
@@ -1,16 +1,19 @@
-import { type RouteConfig, relative ,route ,prefix, layout} from "@react-router/dev/routes";
-import { GlobalConfig } from "app/utils/config.ts";
+import {
+  type RouteConfig,
+  relative,
+  route,
+  prefix,
+  layout,
+} from "@react-router/dev/routes";
+
+import { createBrowserRouter } from "react-router";
+
 const client = relative("app/routes/.client");
 export default [
-    ...prefix(GlobalConfig.prefix,[
-        client.route("/login", "Login/index.tsx"),
-        client.layout('Layout/index.tsx',[
-            
-            client.index("Common/Home/index.tsx"),
-            //client.route("/home","Common/Home/index.tsx"),
-            client.route("/test", "Common/Test/index.tsx"),
-        ])
-    ])
-    
-   
+  client.route("/login", "Login/index.tsx"),
+  client.layout("Layout/index.tsx", [
+    client.index("Common/Home/index.tsx"),
+    //client.route("/home","Common/Home/index.tsx"),
+    client.route("/test", "Common/Test/index.tsx"),
+  ]),
 ] satisfies RouteConfig;
--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from "@react-router/dev/config";
-
 export default {
   future: {
     v8_middleware: true,
   },
+  basename: './',
   //unstable_middleware: true,  // ← 改为这个
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
